bed49564b3c4daa88e76ecaa7ad429ca1e6fe9dc
Yousouf Am <88590585+Amrani90@users.noreply.github.com>
2021-11-08 21:38:52 +0100

    phpresults

--- hello-world.php
+++ hello-world.php
@@ -8,7 +8,42 @@
 </head>
 
 <body>
-<?php echo "Hallo Welt!" ; ?> 
-    
+
+<?= "<h1>Yousouf Amrani!</h1>" ?>
+
+
+
+
+<?php 
+$name = "Yousouf";
+$surname = "AM";
+$age = 30;
+$jobtitle = "Developer";
+echo '<i><h2>My name is</h2></i>' . $name . $surname . '<i><h2>and im</h2></i>' . $age . '<i><h2>years old and i work as a</h2></i>' . $jobtitle;?>
+
+<br>
+<br>
+<br>
+<?php
+
+     
+
+       $numbers = array(10, 20, 30, 40, 50);
+
+
+       $numbers[0] = "Mark";
+       $numbers[1] = "John";
+       $numbers[2] = "Georg";
+       $numbers[3] = "Lisa";
+
+       
+       echo "the first Player in the Team is: " . $numbers[0 ] . "<br/>";
+       echo "the Second Player in the Team is: " . $numbers[1 ] . "<br/>";
+       echo "the Third Player in the Team is: " . $numbers[2 ] . "<br/>";
+       echo "the fourth Player in the Team is:" . $numbers[3 ] . "<br/>";
+?>
+
+
+
 </body>
 </html>
